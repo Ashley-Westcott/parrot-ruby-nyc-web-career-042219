3c830bdbd706c75697600903bc5736fcf01b3206
Ashley Westcott <ashley.e.westcott@gmail.com>
2019-04-03 04:30:50 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,9 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(sound="Squawk!")
-  puts sound
+def parrot(sound)
+  if sound
+    return sound
+  else
+    return "Squawk!"
 end
